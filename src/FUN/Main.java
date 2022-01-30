1754e20aea1b733b97bdb766105f8ec1fc4824c3
sebastian <sebassg8809@gmail.com>
2022-01-29 19:20:38 -0500

    first

---
+++ src/FUN/Main.java
@@ -0,0 +1,13 @@
+package FUN;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Test test=new Test();
+        test.food="fries";
+        test.food="burgers";
+        test.food="pizza";
+        System.out.println(test.food);
+
+    }
+}
---
+++ src/FUN/Test.java
@@ -0,0 +1,11 @@
+package FUN;
+
+public class Test {
+
+        String color;
+        String model;
+        int age;
+        String name;
+        String food;
+        String drinks;
+}
